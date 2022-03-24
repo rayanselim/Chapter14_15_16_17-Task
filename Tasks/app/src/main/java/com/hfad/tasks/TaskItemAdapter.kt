3c0968ed3aa45224ae9fcbad6f772f488b5825a5
rayanselim <rayanselim@gmail.com>
2022-03-24 11:16:20 -0400

    Chapter 17 update

--- Tasks/app/src/main/java/com/hfad/tasks/TaskItemAdapter.kt
+++ Tasks/app/src/main/java/com/hfad/tasks/TaskItemAdapter.kt
@@ -8,13 +8,13 @@ import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.hfad.tasks.databinding.TaskItemBinding
 
-class TaskItemAdapter
+class TaskItemAdapter (val clickListener: (taskId: Long) -> Unit)
     : ListAdapter<Task, TaskItemAdapter.TaskItemViewHolder>(TaskDiffItemCallback()) {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int)
             : TaskItemViewHolder = TaskItemViewHolder.inflateFrom(parent)
     override fun onBindViewHolder(holder: TaskItemViewHolder, position: Int) {
         val item = getItem(position)
-        holder.bind(item)
+        holder.bind(item, clickListener)
     }
     class TaskItemViewHolder(val binding: TaskItemBinding)
         : RecyclerView.ViewHolder(binding.root) {
@@ -25,8 +25,9 @@ class TaskItemAdapter
                 return TaskItemViewHolder(binding)
             }
         }
-        fun bind(item: Task) {
+        fun bind(item: Task, clickListener: (taskId: Long) -> Unit) {
             binding.task = item
+            binding.root.setOnClickListener { clickListener(item.taskId) }
         }
     }
 }
--- Tasks/app/src/main/java/com/hfad/tasks/TasksViewModel.kt
+++ Tasks/app/src/main/java/com/hfad/tasks/TasksViewModel.kt
@@ -1,5 +1,7 @@
 package com.hfad.tasks
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.launch
@@ -8,11 +10,20 @@ class TasksViewModel (val dao: TaskDao) : ViewModel() {
     var newTaskName = ""
     val tasks = dao.getAll()
 
+    private val _navigateToTask = MutableLiveData<Long?>()
+    val navigateToTask: LiveData<Long?>
+        get() = _navigateToTask
+
     fun addTask() {
         viewModelScope.launch {
         val task = Task()
         task.taskName = newTaskName
         dao.insert(task)
         }
+    }fun onTaskClicked(taskId: Long) {
+        _navigateToTask.value = taskId
+    }
+    fun onTaskNavigated() {
+        _navigateToTask.value = null
     }
 }
